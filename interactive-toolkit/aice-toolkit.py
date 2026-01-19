3c9762a549a348073e2338be655cb80967e04e5c
Andrés Megías <109989219+andresmegias@users.noreply.github.com>
2026-01-19 16:39:54 +0100

    Update aice-toolkit.py

--- interactive-toolkit/aice-toolkit.py
+++ interactive-toolkit/aice-toolkit.py
@@ -11,7 +11,7 @@ Andrés Megías.
 
 # AICE parameters.
 aice_labels = ['temp. (K)', 'H2O', 'CO', 'CO2', 'CH3OH', 'NH3', 'CH4']
-weights_path = '/Users/andres/Proyectos/AICE/neural-networks/training/models/aice-lite-weights.npy'
+weights_path = '/Users/andres/Proyectos/AICE/neural-networks/training/models/aice-weights.npy'
 aice_xrange_params = [2100., 3751., 1.]  # /cm
 aice_resolution = 2.  # /cm
 # Matplotlib backend.
