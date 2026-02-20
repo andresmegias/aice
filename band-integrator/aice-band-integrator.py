291700917a061beb3d9c6b4dba602b84623cf80a
Andrés Megías <109989219+andresmegias@users.noreply.github.com>
2026-02-20 21:28:40 +0100

    Add files via upload

--- band-integrator/aice-band-integrator.py
+++ band-integrator/aice-band-integrator.py
@@ -10,13 +10,14 @@ Andrés Megías
 """
 
 # Configuration file.
-config_file = 'NIR38.yaml'
+config_file = 'J110621.yaml'
 
 # Libraries.
 import os
 import sys
-import yaml
+import time
 import platform
+import yaml
 import numpy as np
 import pandas as pd
 import richvalues as rv
@@ -33,21 +34,31 @@ def axis_conversion(x):
         y = 1e4 / x
     return y
 
-def gaussian(x, x0, s, h):
-    """Gaussian with given center (x0), width (s) and height (h)."""
-    y = h * np.exp(-0.5*((x-x0)/s)**2)
+def gaussian(x, height=1, mean=0, std=1, ssf=1):
+    """
+    Apply a Gaussian function to the input array (x), with given height, mean,
+    standard deviation (std) and supersampling factor (ssf).
+    """
+    if ssf == 1:
+        y = height * np.exp(-(x-mean)**2 / (2*std**2))
+    else:
+        dx = x[1] - x[0]
+        x_ = np.linspace(x[0]-dx/2, x[-1]+dx/2, int(ssf*len(x)))
+        y_ = height * np.exp(-(x_-mean)**2 / (2*std**2))
+        y = y_.reshape(-1, ssf).mean(axis=1)
     return y
 
-def correct_water_band_fit(x, y, yf):
-    """Correct left wing of the water band in input spectra and fit (x, y, yf)."""
-    mask = x > 3300.
-    ys = rv.rolling_function(np.median, y, 15)
+def correct_band_fit(x, y, yf, thresh, excluded_regions=[]):
+    """Correct left wing of the band in input spectra and fit (x, y, yf)."""
+    mask = x > thresh
+    for x1x2 in excluded_regions:
+        x1, x2 = min(x1x2), max(x1x2)
+        mask &= (x <= x1) | (x >= x2)
+    ys = rv.rolling_function(np.median, y, size=15)
     s = len(x)*(1.*np.std(ys-y))**2
     interp = UnivariateSpline(x, y, s=s)
     yf[mask] = interp(x[mask])
-    yf[yf < 0.] = 0.
-    yf[x > 3700.] = 0.
-    yf = rv.rolling_function(np.mean, yf, 45)
+    yf = rv.rolling_function(np.mean, yf, size=45)
     return yf
 
 sep = '\\' if platform.system() == 'Windows' else '/'  # folder separator
@@ -58,7 +69,7 @@ plt.close('all')
 print('------------------------------------------')
 print('Automatic Ice Composition Estimator (AICE)')
 print('------------------------------------------')
-print('Band Integrator module')
+print('Band Integrator')
 print()
 
 # Default options.
@@ -70,10 +81,10 @@ default_options = {
     'propagate observational uncertainties': False,
     'do gaussian fit': True,
     'normalize spectra': False,
-    'integration sigmas': 3.,
     'normalization band': 'all',
     'normalization column': 'first',
-    'wavenumber limits': 'auto',
+    'wavenumber limits (/cm)': 'auto',
+    'considered bands': 'all',
     'band labels': {}
     }
 
@@ -103,11 +114,13 @@ do_gaussian_fit = config['do gaussian fit']
 normalize_spectra = config['normalize spectra']
 norm_band = config['normalization band']
 norm_column = config['normalization column']
-sigmas = config['integration sigmas']
-bands = config['bands']
-xlims = config['wavenumber limits']
+xlims = config['wavenumber limits (/cm)']
 band_labels = config['band labels']
-if len(list(bands.keys())) < 2:
+bands = config['all bands']
+considered_bands = config['considered bands']
+if considered_bands == 'all':
+    considered_bands = list(bands.keys())
+if len(considered_bands) < 2:
     show_fractions = False
 # Some more options.
 input_format = '.' + input_file.split('.')[-1]
@@ -142,25 +155,29 @@ elif input_format == '.csv':
     elif norm_column == 'last':
         norm_column = columns[-1]
     dy = np.zeros(len(x))
+    
+t1 = time.time()
 
 # Fitting and integration of bands.
 results = {}
 ylims = []
-for (k,column) in enumerate(columns[1:]):
+for (k, column) in enumerate(columns[1:]):
     y = spectra[column].values
     result = {'bands': {}}
-    for band in bands:
+    for band in considered_bands:
         options = bands[band]
-        center = options['center (/cm)']
-        width = options['width (/cm)']
+        extension = options['extension (/cm)']
+        x1, x2 = min(extension), max(extension)
+        xrange = x2 - x1
+        width = xrange / 6
         band_strength = options['band strength (cm)']
         fix_band = True if 'fixed' in options and options['fixed'] else False
-        do_gaussian_fit_ = (options['do gaussian fit'] if 'do gaussian fit' in options
-                         else do_gaussian_fit)
+        do_gaussian_fit_ = (options['do gaussian fit'] if 'do gaussian fit'
+                            in options else do_gaussian_fit)
         base = options['base level'] if 'base level' in options else 0.
-        remove_baseline = True if 'baseline points' in options else False
+        remove_baseline = True if 'baseline points (/cm)' in options else False
         if remove_baseline:
-            x1x2 = options['baseline points']
+            x1x2 = options['baseline points (/cm)']
             x1, x2 = min(x1x2), max(x1x2)
             y1 = y[np.argmin(np.abs(x-x1))]
             y2 = y[np.argmin(np.abs(x-x2))]
@@ -168,28 +185,31 @@ for (k,column) in enumerate(columns[1:]):
             base += y1 + m*(x-x1)
         else:
             base = base * np.ones(len(x))
-        xrange = [center - width/2, center + width/2]
-        ampl = sigmas * width/2
-        if fix_band:
-            ampl *= 2
-            std = width/2
-        mask = (x >= xrange[0] - ampl) & (x <= xrange[1] + ampl)
-        mask &= np.isfinite(y)
+        x1, x2 = min(extension), max(extension)
+        mask = (x >= x1) & (x <= x2) & np.isfinite(y)
         x_ = x[mask]
         y_ = y[mask]
         dy_ = dy[mask]
         base_ = base[mask]
+        if fix_band:
+            center = options['center (/cm)']
+            std = options['width (/cm)']
+        else:
+            center = x_[np.nanargmax(y_)]
         if do_gaussian_fit_:
-            guess = [np.nanmax(y_)] if fix_band else [center, width/4, np.nanmax(y_)]
+            guess = [np.nanmax(y_)] if fix_band else [center, width, np.nanmax(y_)]
             gaussian_ = ((lambda x,h: gaussian(x, center, std, h)) if fix_band
                          else lambda x,x0,s,h: gaussian(x, x0, s, h))
-            mask = (x_ >= xrange[0]) & (x_ <= xrange[1])
-            if 'excluded width (/cm)' in options:
-                width = options['excluded width (/cm)']
-                semiwidth = width / 2
-                xrange = [center - semiwidth, center + semiwidth]
-                mask &= (x_ <= xrange[0]) | (x_ >= xrange[1])
-            mask &= (y_ - base_ >= 0.1*np.max(y_[mask] - base_[mask]))
+            mask = (x_ >= x1) & (x_ <= x2)
+            if 'excluded region (/cm)' in options:
+                region = options['excluded region (/cm)']
+                if type(region[0]) is not list:
+                    region = [region]
+                for x1x2 in region:
+                    x1, x2 = min(x1x2), max(x1x2)
+                    mask &= (x_ <= x1) | (x_ >= x2)
+            else:
+                region = []
             if propagate_obs_uncs:
                 y_ra = rv.RichArray(y_, dy_)
                 fit_results = rv.curve_fit(x_[mask], y_ra[mask]-base_[mask],
@@ -198,52 +218,71 @@ for (k,column) in enumerate(columns[1:]):
                 params = fit_results['parameters']
                 params_samples = fit_results['parameters samples']
             else:
-                params = curve_fit(gaussian_, x_[mask], y_[mask], p0=guess)[0]
+                params = curve_fit(gaussian_, x_[mask], y_[mask]-base_[mask],
+                                   p0=guess)[0]
+            if (band.startswith('H2O') and 'threshold to correct left part'
+                    in options):
+                correct_band_shape = True
+                thresh = options['threshold to correct left part']
+                if thresh is None:
+                    correct_band_shape = False
+            else:
+                correct_band_shape = False
             if propagate_obs_uncs:
+                if len(x_) > 400:
+                    print('Integrating band...')
                 areas = []
                 if fix_band:
                     for h in params_samples:
                         y_f = gaussian_(x_, h)
-                        if band.startswith('H2O'):
-                            y_f = correct_water_band_fit(x_, y_, y_f)
-                        areas += [np.trapz(y_f, x_)]
+                        if correct_band_shape:
+                            y_f = correct_band_fit(x_, y_, y_f, thresh, region)
+                        areas += [np.trapezoid(y_f, x_)]
                 else:
                     for (x0,s,h) in params_samples:
                         y_f = gaussian_(x_, x0, s, h)
-                        if band.startswith('H2O'):
-                            y_f = correct_water_band_fit(x_, y_, y_f)
-                        areas += [np.trapz(y_f, x_)]
+                        if correct_band_shape:
+                            y_f = correct_band_fit(x_, y_, y_f, thresh, region)
+                        areas += [np.trapezoid(y_f, x_)]
                 area = rv.evaluate_distr(areas, domain=[0.,np.inf],
                                          consider_intervs=False)
             else:
                 y_f = gaussian_(x_, *params)
-                area = np.trapz(y_f, x_)
+                area = np.trapezoid(y_f, x_)
             params = rv.rich_array(params)
-            y_f = gaussian_(x_, *params.centers)
-            if band.startswith('H2O'):
-                y_f = correct_water_band_fit(x_, y_, y_f)
+            if len(x_) < 25:
+                x1, x2 = x_[0], x_[-1]
+                x_new = np.append(x_, np.linspace(x1, x2, 2*len(x_)))
+                x_new = np.unique(x_new)
+                base_ = np.interp(x_new, x_, base_)
+                x_ = x_new
+            y_f = gaussian_(x_, *params.centers) + base_
+            if correct_band_shape:
+                y_f = correct_band_fit(x_, y_, y_f, thresh, region)
         else:
             if propagate_obs_uncs:
                 y_ra = rv.RichArray(y_, dy_)
-                areas = rv.array_distribution(lambda x,y: np.trapz(y-base_,x),
+                areas = rv.array_distribution(lambda x,y: np.trapezoid(y-base_,x),
                                               [x_, y_ra], num_samples=1200,
                                               consider_intervs=False)
                 area = rv.evaluate_distr(areas, domain=[0.,np.inf],
                                          consider_intervs=False)
             else:
-                area = np.trapz(y_-base_, x_)
+                area = np.trapezoid(y_-base_, x_)
         with np.errstate(invalid='ignore'):
             coldens = np.log(10) * area / band_strength
         result['bands'][band] = {}
         if do_gaussian_fit_:
             result['bands'][band]['fit parameters'] = params
-            result['bands'][band]['fit'] = {'x': x_, 'y': y_f+base_, 'base': base_}
+            result['bands'][band]['fit'] = {'x': x_, 'y': y_f, 'base': base_}
         else:
             result['bands'][band]['band'] = {'x': x_, 'y': y_, 'base': base_}
         result['bands'][band]['column density (/cm2)'] = coldens
         results[column] = result
         y_max = max(y.max(), y_f.max()) if do_gaussian_fit_ else y.max()
         ylims += [y.min(), y_max]
+        
+t2 = time.time()
     
 #%% Displaying results.
 
@@ -263,7 +302,7 @@ for column in shown_results:
     print('Column densities:')
     for band in result['bands']:
         colden = result['bands'][band]['column density (/cm2)']
-        print('- {}: {} /cm2'.format(band, colden))
+        print(f'- {band}: {colden} /cm2')
     if show_fractions:
         print('Fractional abundances:')
         coldens = np.array([result['bands'][band]['column density (/cm2)']
@@ -271,10 +310,10 @@ for column in shown_results:
         with np.errstate(invalid='ignore'):
             fractions = coldens / np.sum(coldens)
         for (band, fraction) in zip(result['bands'], fractions):
-            print('- {}: {} %'.format(band, 100*fraction))
+            print(f'- {band}: {100*fraction} %')
     print()
 
-for (k,column) in enumerate(columns[1:]):
+for (k, column) in enumerate(columns[1:]):
     y = spectra[column].values
     if normalize_spectra:
         if output_format == '.csv':
@@ -289,7 +328,13 @@ for (k,column) in enumerate(columns[1:]):
                 norm = np.sum(coldens)
         else:
             norm = result['bands'][norm_band]['column density (/cm2)']
+        norm /= np.log(10)
         results[column]['norm'] = norm
+       
+elapsed_time = t2 - t1
+mins = elapsed_time // 60
+secs = elapsed_time % 60
+print(f'Elapsed time for calculations: {mins} mins + {secs:.4f} s.')
     
 #%% Plotting and optional normalization.
 
@@ -305,28 +350,51 @@ for (k,column) in enumerate(columns[1:]):
     result = results[column]
     plt.figure(k+1, figsize=figsize)
     plt.clf()    
-    plt.errorbar(x, y, dy, ms=1., color='black', ecolor='gray', label='observations')
+    plt.errorbar(x, y, dy, ms=1., color='black', ecolor='gray',
+                 drawstyle='steps-mid', label='observations')
     plt.axhline(y=0, color='k', ls='--', lw=0.6)
     if do_gaussian_fit:
         plt.plot([], color='palevioletred', label='AICE fit')
     plt.fill_between([], [], color='palevioletred', alpha=0.2, label='integrated area')
     std = np.nanstd(y)
+    are_excluded_regions = False
     for band in result['bands']:
-        if 'fit' in  result['bands'][band]:
-            fit_data = result['bands'][band]['fit']
+        band_options = bands[band]
+        band_results = result['bands'][band]
+        if 'fit' in result['bands'][band]:
+            fit_data = band_results['fit']
             x_ = fit_data['x']
             y_ = fit_data['y']
             base_ = fit_data['base']
             plt.plot(x_, y_, color='palevioletred', zorder=3., alpha=0.7)
+            fixed_band = (True if 'fixed' in band_options and band_options['fixed']
+                          else False)
+            center = (band_options['center (/cm)'] if fixed_band
+                      else band_results['fit parameters'][0].main)
+            if 'excluded region (/cm)' in band_options:
+                are_excluded_regions = True
+                region = band_options['excluded region (/cm)']
+                if type(region[0]) is not list:
+                    region = [region]
+                for x1x2 in region:
+                    x1, x2 = min(x1x2), max(x1x2)
+                    for (fc, ec) in zip(['none', 'gray'], ['lightgray', 'none']):
+                        plt.axvspan(x1, x2, facecolor=fc, edgecolor=ec,
+                                    hatch='/', alpha=0.3)
         else:
-            x_ = result['bands'][band]['band']['x']
-            y_ = result['bands'][band]['band']['y']
-            base_ = result['bands'][band]['band']['base']
+            x_ = band_results['band']['x']
+            y_ = band_results['band']['y']
+            base_ = band_results['band']['base']
+            extension = band_options['extension (/cm)']
+            center = np.mean(extension)
         plt.fill_between(x_, base_, y_, color='palevioletred', alpha=0.4)
         y_text = np.max(y_) + 0.5*std
         text = band_labels[band] if band in band_labels else band
-        plt.text(bands[band]['center (/cm)']+34., y_text, text,
+        plt.text(center+34., y_text, text,
                  ha='center', va='center', fontsize=6.)
+    if are_excluded_regions:
+        plt.axvspan(10., 10., facecolor='lightgray', edgecolor='gray',
+                    hatch='/', alpha=0.3, label='excluded regions')
     plt.xlim(xlims)
     plt.axhline(y=0, color='black', lw=0.5)
     plt.xlabel('wavenumber (cm$^{-1}$)', labelpad=6.)
